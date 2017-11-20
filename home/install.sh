af99ee3bf018c09ae373fe6026a736f1e4c51210
Zway <zway8083@gmail.com>
2017-11-20 13:08:11 +0100

    Update crontab installer

--- home/install.sh
+++ home/install.sh
@@ -8,7 +8,3 @@ echo "$LOG" 'Replaced scripts'
 crontab -u "$USER" -r
 crontab -u "$USER" 'crontabs/usercrontab'
 echo "$LOG" 'Updated user crontab'
-
-sudo crontab -u "root" -r
-sudo crontab -u "root" 'crontabs/rootcrontab'
-echo "$LOG" 'Updated root crontab'
