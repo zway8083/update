094b68b99dcb7aa4336bbe69cb118a339090529a
Zway <zway8083@gmail.com>
2017-11-28 15:18:11 +0100

    update crontab

--- env/install.sh
+++ env/install.sh
@@ -4,15 +4,6 @@ LOG='[ENVIRONEMENT VARIABLES]'
 
 REF_FILE='afg_ericsson.sh'
 USR_FILE='/etc/profile.d/'"$REF_FILE"
-#BASHRC="$HOME"'/.bashrc'
-
-#COUNT=$(cat "$BASHRC" | grep -cFx ". ~/$REF_FILE")
-#if [ "$COUNT" -eq 0 ]; then
-#    echo '. ~/.bash_afg_ericsson' >> "$BASHRC"
-#    echo "$LOG" 'Updated ~/.bashrc'
-#else
-#    echo "$LOG" 'Already up-to-date ~/.bashrc'
-#fi
 
 diff -q "$USR_FILE" "$REF_FILE" &>/dev/null
 
@@ -23,4 +14,3 @@ if [ "$?" -gt 0 ]; then
 fi
 echo "$LOG" Already up-to-date "$USR_FILE"
 exit 0
-
